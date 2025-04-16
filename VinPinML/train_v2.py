479fac07520489e2c5b0f6efe973e8826fc39bf4
Nitro 5\Roman <roma25200425@mail.ru>
2025-04-16 23:57:02 +0400

    add test

--- VinPinML/train_v2.py
+++ VinPinML/train_v2.py
@@ -5,7 +5,6 @@ import torch.optim as optim
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
-import numpy as np
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
